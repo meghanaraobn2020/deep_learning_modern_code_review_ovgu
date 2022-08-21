28bc95917350098965e0387a0fbf6ab6a25e9d37
Meghana Rao <76180138+meghanaraobn2020@users.noreply.github.com>
2022-08-21 10:48:29 +0200

    changed the path of the dumps and dataset

--- generate_predictions/generate_predictions/generate_predictions.py
+++ generate_predictions/generate_predictions/generate_predictions.py
@@ -44,10 +44,10 @@ class EvalDataset(torch.utils.data.Dataset):
 
 beam_size = 10
 batch_size = 8
-task = 'code2code: '  # possible options: 'code2code: ', 'code&comment2code: ', 'code2comment: '
-data_dir = "data/dataset/dataset/fine-tuning/new_large/code-to-code/"  # change the path if needed- ../../dataset/fine-tuning/large/code-to-code/"
+task = 'code2comment: '  # possible options: 'code2code: ', 'code&comment2code: ', 'code2comment: '
+data_dir = "data/dataset/dataset/fine-tuning/new_large/code-to-comment/"  # change the path if needed- ../../dataset/fine-tuning/large/code-to-code/"
 tokenizer_name = "deep_learning_modern_code_review_ovgu/tokenizer/tokenizer/TokenizerModel.model" #"../../tokenizer/TokenizerModel.model" 
-model_name_or_path ="deep_learning_modern_code_review_ovgu/dumps/code2code/isr_learning_rate/pytorch_model.bin" #"./dumps/pytorch_model.bin" 
+model_name_or_path ="deep_learning_modern_code_review_ovgu/dumps/code2comment/isr_learning_rate/pytorch_model.bin" #"./dumps/pytorch_model.bin" 
 config_name = "deep_learning_modern_code_review_ovgu/generate_predictions/generate_predictions/config.json" #"./config.json" 
 
 dataset = EvalDataset(data_dir, task)
