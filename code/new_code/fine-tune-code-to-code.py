57793f46d60de10d956f13f952072b296d687b1f
Meghana Rao <76180138+meghanaraobn2020@users.noreply.github.com>
2022-08-30 16:48:18 +0200

    updated model directory path

--- code/new_code/fine-tune-code-to-code.py
+++ code/new_code/fine-tune-code-to-code.py
@@ -56,7 +56,7 @@ vocab_model_path = root_path + "automating_code_review/automating_code_review/to
 vocab_path = root_path + "automating_code_review/automating_code_review/tokenizer/TokenizerModel.vocab"
 
 # Model cehckpoint path
-MODEL_DIR = root_path + "model_checkpoints/" + task_name4 + "/check_" + current_time
+MODEL_DIR = root_path + "model_checkpoints/" + task_name5 + "/check_" + current_time
 if not os.path.exists(MODEL_DIR):
     os.makedirs(MODEL_DIR)
 
