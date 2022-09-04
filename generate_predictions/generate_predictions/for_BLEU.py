763d24244e6f9c3b2346df3de90f03bcf698cf87
Meghana Rao <76180138+meghanaraobn2020@users.noreply.github.com>
2022-09-04 20:15:19 +0200

    updated paths

--- generate_predictions/generate_predictions/for_BLEU.py
+++ generate_predictions/generate_predictions/for_BLEU.py
@@ -16,8 +16,8 @@ for BEAM_SIZE in [10]:
     # - path_statistics : the file where the statistics will be saved
 
     path_targets = root_path + 'data/dataset/dataset/fine-tuning/new_large/code-to-comment/test.tsv'
-    path_predictions = root_path + 'data/dataset/dataset/fine-tuning/new_large/code-to-comment/predictions/isr_learning_rate/predictions_' + str(BEAM_SIZE) + '.txt'
-    path_statistics =  root_path + 'data/dataset/dataset/fine-tuning/new_large/code-to-comment/predictions/isr_learning_rate/statistics_' + str(BEAM_SIZE) + '.txt'
+    path_predictions = root_path + 'data/dataset/dataset/fine-tuning/new_large/code-to-comment/predictions/pre_training_code2comment/isr_learning_rate/predictions_' + str(BEAM_SIZE) + '.txt'
+    path_statistics =  root_path + 'data/dataset/dataset/fine-tuning/new_large/code-to-comment/predictions/pre_training_code2comment/isr_learning_rate/statistics_' + str(BEAM_SIZE) + '.txt'
 
     tgt = [line.strip() for line in open(path_targets)]
     pred = [line.strip() for line in open(path_predictions)]
