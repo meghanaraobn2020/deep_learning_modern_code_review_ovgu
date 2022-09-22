2ff4e6f18e5fbfad615b730f7216b266c337003c
Meghana Rao <76180138+meghanaraobn2020@users.noreply.github.com>
2022-09-22 08:36:38 +0200

    updated paths

--- generate_predictions/generate_predictions/for_BLEU.py
+++ generate_predictions/generate_predictions/for_BLEU.py
@@ -15,9 +15,9 @@ for BEAM_SIZE in [1]:
     # - path_predictions : predictions file
     # - path_statistics : the file where the statistics will be saved
 
-    path_targets = root_path + 'data/dataset/dataset/fine-tuning/new_large/code-to-comment/test.tsv'
-    path_predictions = root_path + 'data/dataset/dataset/fine-tuning/new_large/code-to-comment/predictions/isr_learning_rate/beam_size_1/predictions_' + str(BEAM_SIZE) + '.txt'
-    path_statistics =  root_path + 'data/dataset/dataset/fine-tuning/new_large/code-to-comment/predictions/isr_learning_rate/beam_size_1/statistics_' + str(BEAM_SIZE) + '.txt'
+    path_targets = root_path + 'deep_learning_modern_code_review_ovgu/test_dataset/test_new.tsv'
+    path_predictions = root_path + 'data/dataset/dataset/fine-tuning/new_large/code-to-comment/predictions/pre_training_code2comment/isr_learning_rate/new_data/beam_size_1/predictions_' + str(BEAM_SIZE) + '.txt'
+    path_statistics =  root_path + 'data/dataset/dataset/fine-tuning/new_large/code-to-comment/predictions/pre_training_code2comment/isr_learning_rate/new_data/beam_size_1/statistics_' + str(BEAM_SIZE) + '.txt'
 
     tgt = [line.strip() for line in open(path_targets)]
     pred = [line.strip() for line in open(path_predictions)]
